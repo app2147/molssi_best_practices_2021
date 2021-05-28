8e297e7158a29840de5b05a19b4a5772b299fbb4
Alex Platt <app2147@truman.edu>
2021-05-28 12:23:54 -0500

    adding more test functions
    This was done for the day 2 homework for the MolSSI webinar, functions and test functions were created to calculate (and test) the molecular mass and center of mass for different molecules.

--- molecool/molecule.py
+++ molecool/molecule.py
@@ -2,7 +2,10 @@
 Functions to measure molecules.
 '''
 
+import numpy as np
+
 from .measure import calculate_distance
+from .atom_data import atomic_weights
 
 def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
     
@@ -17,3 +20,57 @@ def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
                 bonds[(atom1, atom2)] = distance
 
     return bonds
+
+def calculate_molecular_mass(symbols):
+    mass = 0
+    for atom in symbols:
+        mass += atomic_weights[atom]
+    return mass
+    
+    """Calculate the mass of a molecule.
+    
+    Parameters
+    ----------
+    symbols : list
+        A list of elements.
+    Returns
+    -------
+    mass : float
+        The mass of the molecule
+    """
+
+def calculate_center_of_mass(symbols, coordinates):
+    """Calculate the center of mass of a molecule.
+   
+    The center of mass is weighted by each atom's weight.
+   
+    Parameters
+    ----------
+    symbols : list
+        A list of elements for the molecule
+    coordinates : np.ndarray
+        The coordinates of the molecule.
+   
+    Returns
+    -------
+    center_of_mass: np.ndarray
+        The center of mass of the molecule.
+
+    Notes
+    -----
+    The center of mass is calculated with the formula
+   
+    .. math:: \\vec{R}=\\frac{1}{M} \\sum_{i=1}^{n} m_{i}\\vec{r_{}i}
+   
+    """
+    total_mass = calculate_molecular_mass(symbols)
+   
+    mass_array = np.zeros([len(symbols), 1])
+   
+    for i in range(len(symbols)):
+        mass_array[i] = atomic_weights[symbols[i]]
+   
+    center_of_mass = sum(coordinates * mass_array) / total_mass
+   
+    return center_of_mass
+    #return np.array([])
--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -4,9 +4,67 @@ Unit and regression test for the molecool package.
 
 # Import package, test suite, and other packages as needed
 import molecool
+import molecool.molecule
+import numpy as np
 import pytest
 import sys
 
 def test_molecool_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "molecool" in sys.modules
+
+def test_calculate_distance():
+    """Testing that calculate_distance calculates what we expect"""
+    
+    r1 = np.array([0,0,0])
+    r2 = np.array([0,1,0])
+
+    expected_distance = 1
+
+    calculated_distance = molecool.calculate_distance(r1, r2)
+
+    assert expected_distance == calculated_distance
+
+def test_calculate_angle():
+    r1 = np.array([0,0,-1])
+    r2 = np.array([0,0,0])
+    r3 = np.array([1,0,0])
+    expected_value = 90
+
+    calculated_value = molecool.calculate_angle(r1,r2,r3,degrees=True)
+
+    assert expected_value == calculated_value
+
+def test_build_bond_list():
+    coordinates = np.array([
+        [1,1,1],
+        [2.4,1,1],
+        [-0.4,1,1],
+        [1,1,2.4],
+        [1,1,-0.4]])
+    
+    bonds = molecool.build_bond_list(coordinates)
+    
+    assert len(bonds) == 4
+
+    for bond_length in bonds.values():
+        assert bond_length == 1.4
+
+def test_molecular_mass():
+    symbols = ['C', 'H', 'H', 'H', 'H']
+    
+    calculated_mass = molecool.molecule.calculate_molecular_mass(symbols)
+    
+    actual_mass = 16.04
+    
+    assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
+
+def test_center_of_mass():
+    symbols = np.array(['C', 'H', 'H', 'H', 'H'])
+    coordinates = np.array([[1,1,1], [2.4,1,1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]])
+
+    center_of_mass = molecool.molecule.calculate_center_of_mass(symbols, coordinates)
+
+    expected_center = np.array([1,1,1])
+
+    assert np.array_equal(center_of_mass, expected_center)
